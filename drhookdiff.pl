be99401a7fc2199e1557a5e048c83fc620499231
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2022-10-26 08:57:52 +0000

    More test cases + drhooklist.pl

--- drhookdiff.pl
+++ drhookdiff.pl
@@ -5,16 +5,22 @@ use DBD::SQLite;
 use Data::Dumper;
 use FileHandle;
 use Term::ANSIColor;
+use Getopt::Long;
 
 use warnings qw (FATAL all);
 use strict;
 
 my $tty = -t STDOUT;
 
-my ($db1, $db2, $Kind, $Limit) = @ARGV;
+my @opts_s = qw (kind limit match where);
+my %opts = qw (kind Self limit 1000);
 
-$Kind ||= 'Self';
-$Limit ||= 1000;
+&GetOptions
+(
+  map ({ ("$_=s", \$opts{$_}) } @opts_s),
+);
+
+my ($db1, $db2) = @ARGV;
 
 my $dbh = 'DBI'->connect ("DBI:SQLite:", '', '', {RaiseError => 1})
   or die ($DBI::errstr);
@@ -23,32 +29,46 @@ $dbh->{RaiseError} = 1;
 $dbh->do ("ATTACH \"$db1\" AS db1;");
 $dbh->do ("ATTACH \"$db2\" AS db2;");
 
+my $MATCH = $opts{match} ? "AND db1.DrHookTime_Merge$opts{kind}.Name REGEXP '$opts{match}'" : "";
+my $WHERE = $opts{where} ? "AND $opts{where}" : "";
+
 my $query = "SELECT 
-              db1.DrHookTime_Merge$Kind.Name                                         AS Name, 
-              db1.DrHookTime_Merge$Kind.Avg                                          AS Avg1, 
-              db2.DrHookTime_Merge$Kind.Avg                                          AS Avg2,
-              db2.DrHookTime_Merge$Kind.Avg - db1.DrHookTime_Merge$Kind.Avg          AS AvgDiff,
-              CASE WHEN db1.DrHookTime_Merge$Kind.Avg > 0 THEN
-              (db2.DrHookTime_Merge$Kind.Avg - db1.DrHookTime_Merge$Kind.Avg) * 100
-            / db1.DrHookTime_Merge$Kind.Avg ELSE 1000 END                            AS AvgIncr
+              db1.DrHookTime_Merge$opts{kind}.Name                                         AS Name, 
+              db1.DrHookTime_Merge$opts{kind}.Avg                                          AS Avg1, 
+              db2.DrHookTime_Merge$opts{kind}.Avg                                          AS Avg2,
+              db2.DrHookTime_Merge$opts{kind}.Avg - db1.DrHookTime_Merge$opts{kind}.Avg    AS AvgDiff,
+              CASE WHEN db1.DrHookTime_Merge$opts{kind}.Avg > 0 THEN
+              (db2.DrHookTime_Merge$opts{kind}.Avg - db1.DrHookTime_Merge$opts{kind}.Avg) * 100
+            / db1.DrHookTime_Merge$opts{kind}.Avg ELSE 1000 END                            AS AvgIncr
             FROM 
-              db1.DrHookTime_Merge$Kind, db2.DrHookTime_Merge$Kind 
-            WHERE db1.DrHookTime_Merge$Kind.Name = db2.DrHookTime_Merge$Kind.Name 
-              AND db2.DrHookTime_Merge$Kind.Avg != db1.DrHookTime_Merge$Kind.Avg 
-            ORDER BY ABS (db1.DrHookTime_Merge$Kind.Avg-db2.DrHookTime_Merge$Kind.Avg) DESC LIMIT $Limit;";
+              db1.DrHookTime_Merge$opts{kind}, db2.DrHookTime_Merge$opts{kind} 
+            WHERE db1.DrHookTime_Merge$opts{kind}.Name = db2.DrHookTime_Merge$opts{kind}.Name 
+              AND db2.DrHookTime_Merge$opts{kind}.Avg != db1.DrHookTime_Merge$opts{kind}.Avg 
+              $MATCH $WHERE
+            ORDER BY ABS (db1.DrHookTime_Merge$opts{kind}.Avg-db2.DrHookTime_Merge$opts{kind}.Avg) DESC LIMIT $opts{limit};";
 
 my $sth = $dbh->prepare ($query);
 
 $sth->execute ();
 
 my @FLD = qw (Name Avg1 Avg2 AvgDiff AvgIncr);
-my %FMT;
+my (%FMT, %HDR);
 @FMT{@FLD} = (qw (%-40s %12.5f %12.5f %+12.5f), '%+12.5f%%');
+@HDR{@FLD} = qw (%-40s %12s %12s %12s %13s);
 
 my $cpm = sub { my ($v, $s) = @_; return &colored ($s, $v > 0. ? 'red' : 'green') };
 
 my %COL = (AvgDiff => $cpm, AvgIncr => $cpm);
 
+
+for my $i (0 .. $#FLD)
+  {
+    my $FLD = $FLD[$i];
+    my $str = sprintf ("$HDR{$FLD}", $FLD);
+    $str = " | $str";
+    print $str;
+    printf (" |\n") if ($i == $#FLD);
+  }
 
 while (my $h = $sth->fetchrow_hashref ())
   {
---
+++ drhooklist.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl -w
+
+use DBI;
+use DBD::SQLite;
+use Data::Dumper;
+use FileHandle;
+use Term::ANSIColor;
+use Getopt::Long;
+
+use warnings qw (FATAL all);
+use strict;
+
+my $tty = -t STDOUT;
+
+my @opts_s = qw (kind limit match where);
+my %opts = qw (kind Self limit 1000);
+
+&GetOptions
+(
+  map ({ ("$_=s", \$opts{$_}) } @opts_s),
+);
+
+my @db = @ARGV;
+
+my $dbh = 'DBI'->connect ("DBI:SQLite:", '', '', {RaiseError => 1})
+  or die ($DBI::errstr);
+$dbh->{RaiseError} = 1;
+
+
+my @i = (0 .. $#db);
+my @dn = map { "db$_" } @i;
+my %dn;
+@dn{@db} = @dn;
+my @avg = map { "Avg$_" } @i;
+
+for my $db (@db) 
+  {
+    $dbh->do ("ATTACH \"$db\" AS $dn{$db};");
+  }
+
+my $MATCH = $opts{match} ? "AND db1.DrHookTime_Merge$opts{kind}.Name REGEXP '$opts{match}'" : "";
+my $WHERE = $opts{where} ? "AND $opts{where}" : "";
+
+my $query = "SELECT db1.DrHookTime_Merge$opts{kind}.Name AS Name, "
+            . join (', ', map ({ "$dn[$_].DrHookTime_Merge$opts{kind}.Avg AS $avg[$_]" } @i))
+            . " FROM " . join (', ', map ({ "$dn[$i].DrHookTime_Merge$opts{kind}" } @i))
+            . " WHERE " . join (' OR ', map ({ my $i = $_; map ({ my $j = $_; "$dn[$i].DrHookTime_Merge$opts{kind}.Avg != $dn[$j].DrHookTime_Merge$opts{kind}.Avg" } (0 .. $i-1)) } @i))
+            . " $MATCH $WHERE LIMIT $opts{limit};";
+
+my $sth = $dbh->prepare ($query);
+
+$sth->execute ();
+
+my @FLD = ('Name', @avg);
+my (%FMT, %HDR);
+@FMT{@FLD} = ('%-40s', ('%12.5f') x scalar (@avg));
+@HDR{@FLD} = ('%-40s', ('%12s') x scalar (@avg));
+
+for my $i (0 .. $#FLD)
+  {
+    my $FLD = $FLD[$i];
+    my $str = sprintf ("$HDR{$FLD}", $FLD);
+    $str = " | $str";
+    print $str;
+    printf (" |\n") if ($i == $#FLD);
+  }
+
+while (my $h = $sth->fetchrow_hashref ())
+  {
+    for my $i (0 .. $#FLD)
+      {
+        my $FLD = $FLD[$i];
+        my $str = sprintf ("$FMT{$FLD}", $h->{$FLD});
+        $str = " | $str";
+        print $str;
+        printf (" |\n") if ($i == $#FLD);
+      }
+  }
